4e80f4d88d69e7ba8f7b039b3b77a28232188db2
rithvik_n <rithvik213@gmail.com>
2019-12-28 14:54:44 -0500

    Mecanum TeleOp Added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumPooja.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import java.util.Arrays;
+
+public class MecanumPooja extends LinearOpMode {
+    DcMotor leftFront = null;
+    DcMotor rightFront = null;
+    DcMotor leftBack = null;
+    DcMotor rightBack = null;
+
+    public void runOpMode() throws InterruptedException {
+        leftFront = hardwareMap.get(DcMotor.class, "lf");
+        rightFront = hardwareMap.get(DcMotor.class, "rf");
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack = hardwareMap.get(DcMotor.class, "lb");
+        rightBack = hardwareMap.get(DcMotor.class, "rb");
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double leftFrontVal = gamepad1.left_stick_y - (gamepad1.left_stick_x) + -gamepad1.right_stick_x;
+            double rightFrontVal = gamepad1.left_stick_y + (gamepad1.left_stick_x) - -gamepad1.right_stick_x;
+            double leftBackVal = gamepad1.left_stick_y + (gamepad1.left_stick_x) + -gamepad1.right_stick_x;
+            double rightBackVal = gamepad1.left_stick_y - (gamepad1.left_stick_x) - -gamepad1.right_stick_x;
+
+            //Move range to between 0 and +1, if not already
+            double[] wheelPowers = {rightFrontVal, leftFrontVal, leftBackVal, rightBackVal};
+            Arrays.sort(wheelPowers);
+            if (wheelPowers[3] > 1) {
+                leftFrontVal /= wheelPowers[3];
+                rightFrontVal /= wheelPowers[3];
+                leftBackVal /= wheelPowers[3];
+                rightBackVal /= wheelPowers[3];
+            }
+            leftFront.setPower(leftFrontVal);
+            rightFront.setPower(rightFrontVal);
+            leftBack.setPower(leftBackVal);
+            rightBack.setPower(rightBackVal);
+        }
+    }
+}
